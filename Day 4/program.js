44639d346bdf957b0dc5ea7a60043271927b7308
Riki Fridrich <riki@fczbkk.com>
2023-03-30 19:28:55 +0200

    vyhodocovani hadaneho cisla

--- Day 4/program.js
+++ Day 4/program.js
@@ -11,13 +11,23 @@ let maxCislo = 100;
 window.addEventListener("load", function() {
 
 	let vygenerovaneCislo = nahodneCislo(minCislo, maxCislo);
+	// console.log('vygenerovane cislo', vygenerovaneCislo)
 
 	let vstupnePolicko = document.querySelector("#number-input");
 	let odesilaciTlacitko = document.querySelector("#submit-button");
+	let komunikacniOkenko = document.querySelector("#message");
 
 	odesilaciTlacitko.addEventListener("click", function() {
-		let hadaneCislo = vstupnePolicko.value;
-		console.log(hadaneCislo)
+		let hadaneCislo = Number(vstupnePolicko.value);
+
+		if (hadaneCislo === vygenerovaneCislo) {
+			komunikacniOkenko.innerHTML = "Gratulujem, je to presne ono."
+		} else if (hadaneCislo < vygenerovaneCislo) {
+			komunikacniOkenko.innerHTML = "Cislo je nizsi nez to, na ktere myslim."
+		} else if (hadaneCislo > vygenerovaneCislo) {
+			komunikacniOkenko.innerHTML = "Cislo je vyssi nez to, na ktere myslim."
+		}
+
 	});
 
 });
