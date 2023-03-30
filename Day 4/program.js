ce1b9cc55662f8c26a11dd74f8bfebbde5827d0b
Riki Fridrich <riki@fczbkk.com>
2023-03-30 18:32:01 +0200

    vygenerovani nahodneho cisla

--- Day 4/program.js
+++ Day 4/program.js
@@ -1,5 +1,19 @@
-// Funkce pro vygenerování náhodného celého čísla mezi hodnotami minimum (včetně) a maximum (včetně).
-function nahodneCislo(minimum, maximum) {
+// Funkce pro vygenerování náhodného celého čísla mezi hodnotami minimum
+// (včetně) a maximum (včetně).
+function nahodneCislo (minimum, maximum) {
 	let vysledek = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 	return vysledek;
 }
+
+let minCislo = 1;
+let maxCislo = 100;
+
+window.addEventListener("load", function() {
+
+	let vygenerovaneCislo = nahodneCislo(minCislo, maxCislo);
+
+	let vstupnePolicko = document.querySelector("#number-input");
+
+	console.log(vstupnePolicko);
+
+});
